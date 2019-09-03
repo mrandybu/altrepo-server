0adbf8d4c72d82bcc1ca8425b43dbac0c1efcbe1
Andrey Bychkov <mrdrew@altlinux.org>
2019-09-03 12:50:53 +0300

    fix sql query in /what_depend_src (loss of first depth request )

--- app.py
+++ app.py
@@ -873,24 +873,29 @@ def broken_build():
 
     server.request_line = \
         "SELECT DISTINCT pkgname, version, release, epoch, serial_, " \
-        "sourcerpm AS filename, assigment_name, groupUniqArray(arch) " \
-        "FROM last_packages INNER JOIN (SELECT DISTINCT pkgname, filename " \
-        "FROM last_depends WHERE dpname IN (SELECT DISTINCT name FROM " \
-        "last_packages WHERE sourcerpm IN (SELECT filename FROM last_packages " \
-        "WHERE name IN (SELECT DISTINCT pkgname FROM last_depends WHERE " \
-        "dpname IN (SELECT DISTINCT name FROM last_packages WHERE sourcerpm " \
-        "IN (SELECT filename FROM last_packages WHERE name IN {name} AND " \
-        "sourcepackage = 1 AND assigment_name = '{branch}') AND " \
-        "assigment_name = '{branch}' AND sourcepackage = 0 AND name NOT LIKE " \
-        "'%-debuginfo') AND sourcepackage = 1 AND assigment_name = '{branch}') " \
-        "AND assigment_name = '{branch}' AND sourcepackage = 1) AND " \
-        "assigment_name = '{branch}' AND name NOT LIKE '%-debuginfo' AND " \
-        "sourcepackage = 0) AND sourcepackage = 1 AND assigment_name = " \
-        "'{branch}') USING filename WHERE assigment_name = '{branch}' AND " \
-        "sourcepackage = 0 {arch} GROUP BY (pkgname, version, release, epoch, " \
-        "serial_, filename, assigment_name)".format(
-            name=input_pkgs, branch=pbranch, arch='{arch}'
-        )
+        "sourcerpm AS filename, assigment_name, groupUniqArray(arch) FROM " \
+        "last_packages INNER JOIN (SELECT pkgname, filename FROM (SELECT " \
+        "pkgname, filename FROM last_depends WHERE dpname IN (SELECT " \
+        "DISTINCT name FROM last_packages WHERE sourcerpm IN (SELECT " \
+        "filename FROM last_packages WHERE name IN {name} AND " \
+        "assigment_name = '{branch}' AND sourcepackage = 1) AND " \
+        "assigment_name = '{branch}' AND sourcepackage = 0 AND name " \
+        "NOT LIKE '%-debuginfo') AND assigment_name = '{branch}' AND " \
+        "sourcepackage = 1 AND dptype = 'require' UNION ALL SELECT pkgname, " \
+        "filename FROM last_depends WHERE dpname IN (SELECT DISTINCT name " \
+        "FROM last_packages WHERE sourcerpm IN (SELECT filename FROM " \
+        "last_depends WHERE dpname IN (SELECT DISTINCT name FROM " \
+        "last_packages WHERE sourcerpm IN (SELECT filename FROM " \
+        "last_packages WHERE name IN {name} AND assigment_name = '{branch}' " \
+        "AND sourcepackage = 1) AND assigment_name = '{branch}' AND " \
+        "sourcepackage = 0 AND name NOT LIKE '%-debuginfo') AND " \
+        "assigment_name = '{branch}' AND sourcepackage = 1 AND " \
+        "dptype = 'require') AND assigment_name = '{branch}' AND " \
+        "sourcepackage = 0 AND name NOT LIKE '%-debuginfo') AND " \
+        "assigment_name = '{branch}' AND sourcepackage = 1)) USING filename " \
+        "WHERE assigment_name = '{branch}' AND sourcepackage = 0 {arch} " \
+        "GROUP BY (pkgname, version, release, epoch, serial_, filename, " \
+        "assigment_name)".format(name=input_pkgs, branch=pbranch, arch='{arch}')
 
     if arch:
         server.request_line = server.request_line.format(
