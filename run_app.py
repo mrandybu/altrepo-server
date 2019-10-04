daf804d0b0b5fa7152a26759b3ed4d308f6fc0f5
Andrey Bychkov <mrdrew@altlinux.org>
2019-10-04 17:53:42 +0300

    fix err of type of parameters in launcher

--- run_app.py
+++ run_app.py
@@ -42,8 +42,6 @@ def start():
                 ('name', namespace.DATABASE_NAME),
                 ('try_numbers', namespace.TRY_CONNECTION_NUMBER),
                 ('try_timeout', namespace.TRY_TIMEOUT),
-                ('user', namespace.DATABASE_USER),
-                ('password', namespace.DATABASE_PASS)
             ],
             'application': [
                 ('host', namespace.DEFAULT_HOST),
