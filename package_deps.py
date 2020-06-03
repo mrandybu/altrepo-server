90cccedf772cc2a1a6fec3977fdc862814b871fa
Andrey Bychkov <mrdrew@altlinux.org>
2020-06-03 17:59:08 +0300

    Add class for searching all build dependencies

---
+++ package_deps.py
@@ -0,0 +1,38 @@
+from logic_server import server
+
+
+class PackageDependencies:
+    def __init__(self, pkgname, pbranch):
+        self.pkgname = pkgname
+        self.pbranch = pbranch
+        self.static_archs = ['x86_64', 'noarch']
+        self.dep_list = []
+
+    def get_package_dep_set(self, first=False, pkgs=None):
+        if first is True:
+            pkgs = \
+                "SELECT pkg.pkghash FROM last_packages WHERE name = '{pkg}' " \
+                "AND assigment_name = '{branch}' AND sourcepackage = 1" \
+                "".format(pkg=self.pkgname, branch=self.pbranch)
+
+        server.request_line = (
+            "SELECT pkg.pkghash FROM last_packages WHERE pkg.pkghash IN ("
+            "SELECT pkghash FROM Depends WHERE dpname IN (SELECT dpname FROM "
+            "Depends WHERE pkghash IN ({pkgs}) AND dptype = 'require') AND "
+            "dptype = 'provide') AND assigment_name = '{branch}' AND "
+            "sourcepackage = 0 AND arch IN {archs}".format(
+                pkgs=pkgs, branch=self.pbranch, archs=tuple(self.static_archs)
+            )
+        )
+
+        status, response = server.send_request()
+        if status is False:
+            return response
+
+        tmp_list = [dep[0] for dep in response if dep[0] not in self.dep_list]
+        if not tmp_list:
+            return self.dep_list
+
+        self.dep_list += tmp_list
+
+        return self.get_package_dep_set(pkgs=tuple(tmp_list))
