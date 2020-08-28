ed91742d2a9bfb892055babc68cb51356f99fa77
Andrey Bychkov <mrdrew@altlinux.org>
2020-08-28 17:19:58 +0300

    /unpackaged_dirs: replace incorrect sql query to get unpacked directories

--- sql.d/unpackaged_dirs/get_pkg_dirs.sql
+++ sql.d/unpackaged_dirs/get_pkg_dirs.sql
@@ -1,49 +1,41 @@
-SELECT name,
-       version,
-       release,
-       Df.name,
-       Df.version,
-       Df.release
-FROM
-  (SELECT name,
-          version,
-          release
-   FROM last_packages
-   WHERE assigment_name = %(pkgset1)s
-     AND sourcepackage = 1
-     AND (name,
-          version,
-          release) NOT IN
-       (SELECT name,
-               version,
-               release
-        FROM last_packages
-        WHERE assigment_name = %(pkgset2)s
-          AND sourcepackage = 1)
-     AND name IN
-       (SELECT name
-        FROM last_packages
-        WHERE assigment_name = %(pkgset2)s
-          AND sourcepackage = 1)) AS PkgSet2
-INNER JOIN
-  (SELECT name,
-          version,
-          release
-   FROM last_packages
-   WHERE assigment_name = %(pkgset2)s
-     AND sourcepackage = 1) AS Df USING name
-UNION ALL
-SELECT name,
-       version,
-       release,
-       '',
-       '',
-       ''
-FROM last_packages
-WHERE assigment_name = %(pkgset1)s
-  AND sourcepackage = 1
-  AND name NOT IN
-    (SELECT name
-     FROM last_packages
-     WHERE assigment_name = %(pkgset2)s
-       AND sourcepackage = 1)
+SELECT DISTINCT Pkg.pkgname,
+                extract(filename, '^(.+)/([^/]+)$') AS dir,
+                Pkg.version,
+                Pkg.release,
+                Pkg.epoch,
+                Pkg.packager,
+                Pkg.packager_email,
+                groupUniqArray(Pkg.arch)
+FROM File
+         LEFT JOIN (
+    SELECT pkghash,
+           name as pkgname,
+           version,
+           release,
+           epoch,
+           disttag,
+           packager_email,
+           packager,
+           arch
+    FROM Package) AS Pkg USING pkghash
+WHERE empty(fileclass)
+  AND (pkghash IN (SELECT pkghash
+                   FROM last_packages
+                   WHERE (assigment_name = %(branch)s)
+                     AND packager_email
+                       LIKE %(email)s
+                     AND (sourcepackage = 0)
+                     AND (arch IN %(archs)s)))
+  AND (hashdir NOT IN (SELECT hashname
+                       FROM File
+                       WHERE (fileclass =
+                              'directory')
+                         AND (pkghash IN (SELECT pkghash
+                                          FROM last_packages
+                                          WHERE (assigment_name = %(branch)s)
+                                            AND (sourcepackage = 0)
+                                            AND (arch IN
+                                                 %(archs)s)))))
+GROUP BY (Pkg.pkgname, dir, Pkg.version, Pkg.release,
+          Pkg.epoch, Pkg.packager, Pkg.packager_email)
+ORDER BY packager_email
