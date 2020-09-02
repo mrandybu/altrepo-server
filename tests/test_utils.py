ed86167713ec74ddf47725949f7b24055b921404
Andrey Bychkov <mrdrew@altlinux.org>
2020-09-02 15:32:02 +0300

    Update test module for utils testing

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,21 +1,27 @@
-import unittest
+import os
 import json
 import time
+import logging
+import unittest
+import tempfile
+from io import StringIO
+
 import utils
 from utils import func_time
-from paths import paths
+
+log_stream = StringIO()
+logging.basicConfig(stream=log_stream, level=logging.INFO)
 
 
 class TestUtils(unittest.TestCase):
-    def test_logger(self):
+    def test_get_logger(self):
         logger = utils.get_logger(__name__)
         message = "logging from {}".format(__name__)
         logger.info(message)
 
-        with open(paths.LOG_FILE) as log:
-            assert message in log.read()
+        assert message in log_stream.getvalue()
 
-    def test_exception_logger(self):
+    def test_exception_to_logger(self):
         try:
             1 / 0
         except Exception as err:
@@ -23,16 +29,20 @@ class TestUtils(unittest.TestCase):
 
     def test_read_config(self):
         contain = """
-        [TestConfig]
-        Field1 = value1
-        Field2 = value2
-        Field3 = value3
-        """
-        file = "/tmp/{}".format(__name__)
-        with open(file, "w") as f:
-            f.write(contain)
+                [TestConfig]
+                Field1 = value1
+                Field2 = value2
+                Field3 = value3
+                """
 
-        config = utils.read_config(file)
+        tmp = tempfile.NamedTemporaryFile(delete=False)
+        tmp.write(contain.encode())
+        tmp.close()
+
+        path = os.path.join(os.path.dirname(tmp.name), tmp.name)
+        config = utils.read_config(path)
+
+        os.remove(path)
 
         assert 'value1' == config.get('TestConfig', 'Field1')
         assert 'value2' == config.get('TestConfig', 'Field2')
@@ -60,13 +70,23 @@ class TestUtils(unittest.TestCase):
         assert 'elem3.2' == js['2']['key3']
 
     def test_func_time(self):
-        logger = utils.get_logger(__name__)
+        logger = logging.getLogger()
 
         @func_time(logger)
-        def sleep_fuc():
+        def sleep_func():
             time.sleep(2)
 
-        sleep_fuc()
+        sleep_func()
 
-        with open(paths.LOG_FILE) as f:
-            assert 'Time sleep_fuc is 2.' in f.read()
+        assert 'Time sleep_func is 2.' in log_stream.getvalue()
+
+    def test_tuplelist_to_dict(self):
+        assert {1: [2], 2: [3, 4], 7: [8, 9]} == utils.tuplelist_to_dict(
+            ((1, 2), (2, 3, 4, 6), (7, 8, 9)), 2
+        )
+        assert {1: [2, 3, 4, 5], 6: [7, 8], 9: [0, 1, 2]} == utils.tuplelist_to_dict(
+            ((1, 2, 3, 4, 5), (6, 7, 8), (9, 0, 1, 2)), 4
+        )
+        assert {1: [2, 3, 4], 3: []} == utils.tuplelist_to_dict(
+            ((1,), (1, 2, 3, 4), (3,)), 6
+        )
